757ba8449be7adab7311c43657cce78423371eba
kwon-beom-jun <qjawns0618@naver.com>
2023-09-18 23:55:33 +0900

    study

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,17 +25,37 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    // private final MyLogger myLogger; 에러 발생
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     /**
      * HttpServletRequest : JAVA에서 제공하는 표준 서블릿 규약
      */
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
-        String requestURL = request.getRequestURI().toString();
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURI = request.getRequestURI().toString(); // 값 : /log-demo
+        String requestURL = request.getRequestURL().toString(); // 값 : http://localhost:8080/log-demo
+
+        System.out.println("requestURI : " + requestURI);
+        System.out.println("requestURL : " + requestURL);
+
+        /**
+         * <br> Provider을 사용하여 request scope 빈의 생성을 지연
+         * <br> (스프링 컨테이너에게 요청을 지연하는것이고 스프링 빈이 그 시점에 생성해주는것)
+         * <br>     → ObjectProvider.getObject 호출하는 시점에 HTTP 요청이 진행중이므로
+         * <br>       request scope 빈의 생성이 정상처리
+         * <br>
+         * <br> ObjectProvider.getObject()를 LogDemoController, LogDemoService에서
+         * <br> 각각 한번씩 따로 호출해도 같은 HTTP 요청이면 같은 스프링 빈이 반환
+         * <br>     ex) 코드 실행시 콘솔 로그
+         * <br>         request scope bean create : 참조값 == request scope bean close : 참조값
+         */
+        MyLogger myLogger = myLoggerObjectProvider.getObject(); // 추가
+
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
